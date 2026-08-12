44870a7f3b07c8d7ee4dcf0d23aefa69c71d7f05
goyalriya207 <goyalriya207@gmail.com>
2026-08-12 21:50:45 +0530

    solved moved zeroes

---
+++ Arrays/283-move-zeroes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+
+        int c=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[c]);
+                c++;
+            }
+        }
+
+        for(int val:nums){
+            cout << val;
+        }
+
+        // using 2 pointer
+
+
+        
+    }
+};
