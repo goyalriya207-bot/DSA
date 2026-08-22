99d780fe2efa72a2b50613ef1c8cf260454bb15c
goyalriya207 <goyalriya207@gmail.com>
2026-08-22 22:11:40 +0530

    solved no.s smaller than currnet no.

---
+++ Arrays/1365-no. smaller than current.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+
+       /* vector<int> ans;
+
+        int count;
+
+        for(int i=0;i<nums.size();i++){
+            
+            count=0;
+                                                       brute force:O(N*N)
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]<nums[i]){
+                    count++;
+                }
+            }
+
+            ans.push_back(count);
+        }
+
+        return ans;*/
+
+
+        vector<int> ans;              //concept of hashing 
+        
+    }
+};
